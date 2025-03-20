1d69dece4a06555f94ff5cb5fc1f917229f739fb
JC54695 <clis0022@student.acitech.org>
2025-03-20 09:13:49 -0400

    JS is not very nice :(

---
+++ script.js
@@ -0,0 +1,15 @@
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
+const button4 = document.getElementById("button4");
+const button5 = document.getElementById("button5");
+
+
+
+
+const game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+    let moves = 0;
+
+}
