16390a9d5d1b5184214bb7556842670ed7437f9e
JC54695 <clis0022@student.acitech.org>
2025-03-21 09:13:07 -0400

    JS is making me very very VERY mad :(

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const button1 = document.getElementById("button1");
-const button2 = document.getElementById("button2");
-const button3 = document.getElementById("button3");
-const button4 = document.getElementById("button4");
-const button5 = document.getElementById("button5");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const stapler = document.getElementById("stapler");
+const fireEXT = document.getElementById("fireEXT");
+
+rock.addEventListener("click", game )
+paper.addEventListener("click", game)
 
 
-
-
-const game = () => {
-    let playerScore = 0;
-    let computerScore = 0;
-    let moves = 0;
+function game () {
+    let rock = 1
+    let paper = 2
 
 }
+
